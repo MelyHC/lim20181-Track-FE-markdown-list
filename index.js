a1930a37cee6850b168e559b175d98e9fa622fd0
MelyHC <mely.hidalgo.crespo@gmail.com>
2018-08-17 20:03:04 -0500

    Leer archivo y carpeta

--- index.js
+++ index.js
@@ -4,21 +4,38 @@ const fs = require('fs');
 // const path = require('path');
 const [, , ...args] = process.argv
 
-const readArchFile = () => {
-  fs.stat(`${args}`, (err, data) => {
-    if (data.isFile()) {
-      fs.readFile(`${args}`, (err, data) => {
+// const checkRoute = (directory) => {
+//   try {
+//     process.chdir(directory);
+//     // console.log(process.cwd())
+//     readArchFile(directory);
+//   } catch (err) {
+//     console.log('La ruta no se encontró');
+//   }
+// }
+
+const readArchFile = (argument) => {
+  // console.log(route)
+  fs.stat(argument, (err, stats) => {
+    if (stats.isFile()) {
+      fs.readFile(argument, (err, data) => {
         if (err) throw err;
         console.log(data.toString());
       });
-    } else if (data.isDirectory()) {
-      fs.readdir(`${args}`, (err, files) => {
+    }
+    if (stats.isDirectory()) {
+      fs.readdir(argument, (err, files) => {
         if (err) throw err;
-        console.log(files.toString());
+        // console.log(files);
+        for (let i = 0; i < files.length; i++) {
+          readArchFile(argument + '/' + files[i])
+        }
+        // readArchFile(files[0])
       });
     }
   });
 
 }
 
-readArchFile();
+// checkRoute(args[0]);
+readArchFile(args[0]);
