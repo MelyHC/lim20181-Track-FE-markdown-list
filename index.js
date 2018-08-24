2026ced367262e57dfe51113c2965d0969947c77
MelyHC <mely.hidalgo.crespo@gmail.com>
2018-08-23 19:01:46 -0500

    Actualizacion md-links close #10, close #11, close #17, close #7

--- index.js
+++ index.js
@@ -3,41 +3,30 @@
 const fs = require('fs');
 const path = require('path');
 const program = require('commander');
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
 const marked = require('marked')
-
-const options = {
-  validate: program.validate,
-  stats: program.stats
-}
-
-const createObject = (route, url, description) => {
-  return new object(href = url,
-    text = description,
-    file = route
-  );
-}
 
 const checkFileMd = (nameFile) => {
   const extMd = /\.(md|mkdn|mdown|markdown?)$/i;
   return extMd.test(path.extname(nameFile))
 }
 
-const readFile = (file) => {
-  // console.log(file)
+const readFileMd = (file, links) => {
   fs.readFile(file, 'utf8', (err, data) => {
     if (err) throw err;
-    const newStructureMd = marked(data);
-    // let arrTextUrl = str.match(expLink)
-    const splitMd = newStructureMd.split('<');
-    splitMd.forEach( lines => {
-      const linesLink = lines.match('a ')
-      console.log(linesLink)
-    }) 
+    const renderer = new marked.Renderer()
+    renderer.link = (href, title, text) => {
+      links.push({
+        href: href,
+        text: text,
+        file: file
+      })
+    }
+    marked(data, { renderer: renderer })
   })
 }
 
-const travelArchFile = (routeArchOrFile) => {
+const travelArchFile = (routeArchOrFile, links) => {
   const arrFilesMd = [];
   fs.stat(routeArchOrFile, (err, stats) => {
     if (err) throw err;
@@ -45,14 +34,14 @@ const travelArchFile = (routeArchOrFile) => {
       const fileMd = checkFileMd(routeArchOrFile);
       if (fileMd) {
         // arrFilesMd.push(routeArchOrFile)
-        readFile(routeArchOrFile);
+        readFileMd(routeArchOrFile, links);
         // const arrFilesMd = filesMd.push(routeArchOrFile);
       }
     } else if (stats.isDirectory()) {
       fs.readdir(routeArchOrFile, (err, files) => {
         if (err) throw err;
         files.forEach(file => {
-          travelArchFile(path.join(`${routeArchOrFile}`, `${file}`));
+          travelArchFile(path.join(`${routeArchOrFile}`, `${file}`), links);
         });
       });
     }
@@ -61,15 +50,38 @@ const travelArchFile = (routeArchOrFile) => {
 }
 
 const mdlinks = (route, options) => {
-  // return new Promise((resolve, reject) => {
-
-  // })
-  const travelFile = travelArchFile(path.resolve(route));
+  return new Promise((resolve, reject) => {
+    const links = [];
+    travelArchFile(path.resolve(route), links);
+    setTimeout(() => {
+      resolve(links)
+    }, 500);
+  })
 }
-// checkRoute(args[0]);
-// readArchFile(args[0]);
+
+/* const test = async (route, options) => {
+  await mdlinks(route, options);
+} */
+
 program
+  .arguments('file')
   .option('-v, --validate', 'Validar links si estan rotos o no')
   .option('-s, --stats', 'Mostrar stats de los links')
   .action(mdlinks)
   .parse(process.argv);
+
+const options = {
+  validate: program.validate,
+  stats: program.stats
+}
+
+/* test(program.args[0], options).then((result) => {
+  console.log(result);
+}) */
+
+mdlinks(program.args[0], options)
+  .then(arrLinks => {
+    arrLinks.forEach(link => {
+      console.log(`${link.file}  ${link.href}  ${link.text}`)
+    })
+  })
