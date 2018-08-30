eaf2fdab7542872cf790498b0a1219a71efa4c73
MelyHC <mely.hidalgo.crespo@gmail.com>
2018-08-29 19:30:37 -0500

    Cambio de un array a un string

--- cli.js
+++ cli.js
@@ -21,5 +21,21 @@ if (!route) {
   console.log('Debes ingresar la ruta de un archivo o carpeta')
 } else {
   mdLinks(route, options)
-    .then(arrLinks => console.log(arrLinks))
+    .then(arrLinks => {
+      if (options.stats && options.validate) {
+        console.log(`Total: ${arrLinks.total} \nUnique: ${arrLinks.unique} \nBroken: ${arrLinks.broken} `);
+      } else if (options.stats) {
+        console.log(`Total: ${arrLinks.total} \nUnique: ${arrLinks.unique}`);
+      } else if (options.validate) {
+        arrLinks.forEach(linksData => {
+          console.log(`${linksData.file}\t ${linksData.href}\t ${linksData.statusText}\t ${linksData.status} \t ${linksData.text}`)
+        });
+      } else if (Array.isArray(arrLinks)) {
+        arrLinks.forEach(linksData => {
+          console.log(`${linksData.file}\t ${linksData.href}\t ${linksData.text}`);
+        });
+      } else {
+        console.log(arrLinks);
+      }
+    })
 }
--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 const fs = require('fs');
 const path = require('path');
-const marked = require('marked')
+const marked = require('marked');
 const fetch = require('node-fetch');
 
 const checkFileMd = (nameFile) => {
   const extMd = /\.(md|mkdn|mdown|markdown?)$/i;
-  return extMd.test(path.extname(nameFile))
+  return extMd.test(path.extname(nameFile));
 }
 
 const readFileMd = (arrFiles) => {
-  const links = []
+  const links = [];
   arrFiles.forEach(file => {
     const dataFile = fs.readFileSync(file, 'utf8');
-    const renderer = new marked.Renderer()
+    const renderer = new marked.Renderer();
     renderer.link = (href, title, text) => {
       links.push({
         href: href,
         text: text,
         file: file
-      })
-    }
+      });
+    };
     marked(dataFile, { renderer });
   })
-  return links
+  return links;
 }
 
 const travelArchFile = (routeArchOrFile) => {
@@ -34,7 +34,7 @@ const travelArchFile = (routeArchOrFile) => {
       arrRoute.push(routeArchOrFile);
     }
   } else if (statFileArch.isDirectory()) {
-    const files = fs.readdirSync(routeArchOrFile)
+    const files = fs.readdirSync(routeArchOrFile);
     files.forEach(file => {
       arrRoute = arrRoute.concat(travelArchFile(path.join(`${routeArchOrFile}`, `${file}`)));
     });
@@ -42,24 +42,20 @@ const travelArchFile = (routeArchOrFile) => {
   return arrRoute;
 }
 
-const linksUnique = (arrLinks) => {
-  return [...new Set(arrLinks.map(link => link.href))];
-}
+const linksUnique = (arrLinks) => [...new Set(arrLinks.map(link => link.href))];
 
-const linksBroken = (arrLinksValidate) => {
-  return arrLinksValidate.filter(link => link.status >= 400)
-}
+const linksBroken = (arrLinksValidate) => arrLinksValidate.filter(link => link.status >= 400);
 
 const validateStats = (arrObjLinks) => {
-  const arrLinks = arrObjLinks.map(link => link.href)
-  const promises = arrLinks.map(link => fetch(link))
+  const arrLinks = arrObjLinks.map(link => link.href);
+  const promises = arrLinks.map(link => fetch(link));
   return Promise.all(promises)
     .then(response => {
       const linksValidate = arrObjLinks.map((objLinkData, statsLink) => {
-        objLinkData.status = response[statsLink].status
-        objLinkData.statusText = response[statsLink].statusText
-        return objLinkData
-      })
+        objLinkData.status = response[statsLink].status;
+        objLinkData.statusText = response[statsLink].statusText;
+        return objLinkData;
+      });
       return linksValidate;
     })
 }
@@ -74,17 +70,17 @@ const mdLinks = (route, options) => {
       }
       if (options.validate && options.stats) {
         validateStats(arrLinksMd)
-          .then(response => resolve({ Total: response.length, Unique: linksUnique(response).length, Broken: linksBroken(response).length }))
+          .then(response => resolve({ total: response.length, unique: linksUnique(response).length, broken: linksBroken(response).length }));
       } else if (options.validate) {
         validateStats(arrLinksMd)
-          .then(response => resolve(response))
+          .then(response => resolve(response));
       } else if (options.stats) {
-        resolve({ Total: arrLinksMd.length, Unique: linksUnique(arrLinksMd).length })
+        resolve({ total: arrLinksMd.length, unique: linksUnique(arrLinksMd).length });
       } else {
         resolve(arrLinksMd);
       }
     } else {
-      resolve(`La ruta colocada no se encuentra ${path.resolve(route)}`)
+      resolve(`La ruta colocada no se encuentra ${path.resolve(route)}`);
     }
   })
 }
